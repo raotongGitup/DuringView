9b26987775addf3b6ae7ab994a7f27aa93d92123
lihao <1037891202@qq.com>
2018-05-02 15:23:40 +0800

    v 1.0.1上传

--- app/src/main/java/com/example/dhf/duringview/satelliteSpiderView/ScroolView.java
+++ app/src/main/java/com/example/dhf/duringview/satelliteSpiderView/ScroolView.java
@@ -10,6 +10,7 @@ import android.graphics.Shader;
 import android.support.annotation.Nullable;
 import android.text.TextPaint;
 import android.util.AttributeSet;
+import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
 
@@ -28,7 +29,6 @@ public class ScroolView extends View {
 
     /**
      * y轴高度
-     *
      */
     private int hight;
     Paint paint;
@@ -74,6 +74,11 @@ public class ScroolView extends View {
     private String pointColor = "#ffffff";
     /*没个x轴之间的间隔数*/
     private int xinterval;
+
+    /*是否添加渐变色*/
+    private boolean gradient = false;
+    /*是否需要背景十字架*/
+    private boolean backcross = false;
 
     private Paint colorPaint;
     private Path colorPath;
@@ -258,7 +263,10 @@ public class ScroolView extends View {
 
             } else {
                 canvas.drawText(xinterval * (i + 1) + "", dpToPx(7), hight - dpToPx(20) - yhight * i, textPaint);
-                canvas.drawLine(dpToPx(20), hight - dpToPx(20) - yhight * i, with * pointFS.size(), hight - dpToPx(20) - yhight * i, yPaint);
+                if (backcross) {
+                    canvas.drawLine(dpToPx(20), hight - dpToPx(20) - yhight * i, with * pointFS.size(), hight - dpToPx(20) - yhight * i, yPaint);
+                }
+
             }
         }
         textPaint.setColor(Color.parseColor(xzhiTitleColor));
@@ -268,7 +276,10 @@ public class ScroolView extends View {
                 canvas.drawText(pointFS.get(i).x + "", dpToPx(20) - dpToPx(5), hight - dpToPx(10), textPaint);
             } else {
                 canvas.drawText(pointFS.get(i).x + "", dpToPx(20) + i * with - dpToPx(5), hight - dpToPx(10), textPaint);
-                canvas.drawLine(dpToPx(20) + i * with, hight - dpToPx(20), dpToPx(20) + i * with, dpToPx(70), yPaint);
+                if (backcross) {
+                    canvas.drawLine(dpToPx(20) + i * with, hight - dpToPx(20), dpToPx(20) + i * with, dpToPx(70), yPaint);
+                }
+
             }
         }
 /**
@@ -288,21 +299,23 @@ public class ScroolView extends View {
         paint.setColor(Color.YELLOW);
         paint.setStrokeWidth(3);
         Path path = new Path();
+        colorPath.moveTo(dpToPx(20), hight - dpToPx(21));
         for (int i = 0; i < pointFS.size(); i++) {
             float x = i * with + dpToPx(20);
             float y = hight - dpToPx(20) - (((hight - dpToPx(50) - dpToPx(20)) / (max - main)) * pointFS.get(i).y);
             if (i == 0) {
                 path.moveTo(x, y);
-                colorPath.moveTo(x, y);
+
             } else {
                 path.lineTo(x, y);
-                colorPath.lineTo(x, y);
-
 
             }
+            colorPath.lineTo(x, y);
             circl.add(new PointF(x + "", y));
             canvas.drawCircle(x, y, 8, paintCire);
         }
+        colorPath.lineTo(pointFS.size() * with + dpToPx(20), hight - dpToPx(21));
+        colorPath.close();
         /**
          *
          * 渐变色的运用
@@ -311,11 +324,17 @@ public class ScroolView extends View {
                 Color.parseColor("#B9E731"), Color.WHITE}, null, Shader.TileMode.CLAMP);
         colorPaint.setShader(mShader);
         canvas.drawPath(path, paint);
-        canvas.drawPath(colorPath, colorPaint);
+
+        if (gradient) {
+            canvas.drawPath(colorPath, colorPaint);
+        }
+
     }
 
     //设置scrollerView的滚动条的位置，通过位置计算当前的时段
     public void setScrollOffset(int offset, int maxScrollOffset) {
+
+        Log.e("2222222", "setScrollOffset: " + offset + "最大" + maxScrollOffset);
 
         invalidate();
     }
@@ -441,6 +460,14 @@ public class ScroolView extends View {
 
     public void setPointColor(String pointColor) {
         this.pointColor = pointColor;
+    }
+
+    public void setGradient(boolean gradient) {
+        this.gradient = gradient;
+    }
+
+    public void setBackcross(boolean backcross) {
+        this.backcross = backcross;
     }
 }
 
