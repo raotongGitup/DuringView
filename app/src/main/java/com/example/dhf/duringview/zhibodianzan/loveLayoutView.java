96debca4e8b04ef9b9e65bee2cce8edc0bbc1892
lihao <1037891202@qq.com>
2018-06-05 09:51:08 +0800

    v 1.0.1上传

---
+++ app/src/main/java/com/example/dhf/duringview/zhibodianzan/loveLayoutView.java
@@ -0,0 +1,28 @@
+package com.example.dhf.duringview.zhibodianzan;
+
+import android.content.Context;
+import android.support.annotation.Nullable;
+import android.util.AttributeSet;
+import android.view.View;
+
+/**
+ * Created by ${raotong} on 2018/5/11.
+ */
+
+public class loveLayoutView extends View {
+
+
+    public loveLayoutView(Context context) {
+        super(context);
+    }
+
+    public loveLayoutView(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public loveLayoutView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+
+}
